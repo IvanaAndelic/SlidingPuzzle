d9b674f0980804967a89e3e88923e41cf6a30c60
ivana andelic <ivana.apneli@gmail.com>
2021-06-12 14:56:58 +0200

    commit #1

---
+++ SlidingPuzzle/ChildView.cpp
@@ -0,0 +1,72 @@
+
+// ChildView.cpp : implementation of the CChildView class
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "SlidingPuzzle.h"
+#include "ChildView.h"
+#include "resource.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+	ON_WM_LBUTTONDOWN()
+END_MESSAGE_MAP()
+
+
+
+// CChildView message handlers
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this); // device context for painting
+	
+	CPaintDC dc(this);
+	CBitmap b; b.LoadBitmap(IDB_BITMAP1);
+	CDC memdc; memdc.CreateCompatibleDC(&dc);
+	auto prev = memdc.SelectObject(&b);
+	BITMAP bmp; b.GetBitmap(&bmp);
+	dc.BitBlt(0, 0, bmp.bmWidth, bmp.bmWidth, &memdc, 0, 0, SRCCOPY);
+	dc.SelectObject(prev);
+}
+
+
+
+
+
+
+void CChildView::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+
+	CWnd::OnLButtonDown(nFlags, point);
+}
